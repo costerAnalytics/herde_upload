dc9d14de44a54c109982f7a21ba876df1612db27
Roelof <roelof@costeranalytics.nl>
2022-04-07 20:23:35 +0200

    Log file name

--- neue_dateien_schreiben.r
+++ neue_dateien_schreiben.r
@@ -24,7 +24,7 @@ for(variabele in c(
 	'pg_host',
 	'pg_dbname',
 	'pg_user',
-	'pg_pwd', 
+	'pg_pwd',
 	'pg_schema',
 	'betrieb',
 	'gbak',
@@ -46,8 +46,8 @@ zip_file <- paste0(herde_backup_folder, zip_file)
 unzip(zip_file, overwrite = TRUE, exdir = temp_herde_folder)
 system(paste0(Sys.getenv('gbak'), ' -PAS ', Sys.getenv('herde_pwd'), ' -USER ', Sys.getenv('herde_uid'), ' -REP -c "', temp_herde_folder, '\\HerdeW.fbk" "', temp_herde_folder, '\\', Sys.getenv('herde_backup_db'), '.fdb"'))
 
-odbc_herde <- odbcConnect(Sys.getenv('herde_backup_db'), 
-	uid = Sys.getenv('herde_uid'), pwd = Sys.getenv('herde_pwd'), 
+odbc_herde <- odbcConnect(Sys.getenv('herde_backup_db'),
+	uid = Sys.getenv('herde_uid'), pwd = Sys.getenv('herde_pwd'),
 	case = 'toupper', DBMSencoding ='utf8')
 
 pgdb <- dbConnect(Postgres(), host = Sys.getenv('pg_host'),
@@ -65,6 +65,8 @@ querherde <- function(...) sqlQuery(odbc_herde, paste0(...))
 
 monaten_neu <- NULL # data.frame mit (Jahr, Monat) die neu sind.
 
+log_to_file('Herde file: ', zip_file)
+
 for(tabel in tabellen){
 	cat(tabel)
 	tryCatch({
@@ -76,32 +78,32 @@ for(tabel in tabellen){
 		  log_to_file(paste0(tabel, ': 0 neue Spalten'))
 		  next # naechste Tabel, weil es keine neue Dateien gibt.
 		}
-		
+
 		kolommen <- sqlColumns(odbc_herde, tabel)
 		kolommen <- kolommen %>% select(kolom = COLUMN_NAME, type = TYPE_NAME)
 		kolommen <- kolommen %>% filter(!grepl('BLOB', type))
 		query <- paste(kolommen$kolom, collapse = ',')
-		query <- paste('select ', query, ' from ', tabel, ' 
+		query <- paste('select ', query, ' from ', tabel, '
 			where id > ', max_alt_id, ' order by id')
 		dat <- querherde(query)
-		
-	  
-		# Richtige Type fuer jede Zeile: 
+
+
+		# Richtige Type fuer jede Zeile:
 		integers <- kolommen$kolom[kolommen$type %in% c('BIGINT','INTEGER', 'SMALLINT')]
 		datums <- kolommen$kolom[kolommen$type == 'DATE']
 		for(k in integers) dat[,k] <- as.integer(dat[,k])
 		for(k in datums) dat[,k] <- as.Date(dat[,k])
-		
+
 		colnames(dat) <- tolower(colnames(dat))
-		
+
 		if(tabel != 'hw_besamung' && tabel != 'hw_tu' && !is.null(dat$datum)){
 		  neue_monaten_aux <- as.data.frame(unique(cbind(year(dat$datum), month(dat$datum))))
 		  colnames(neue_monaten_aux) <- c('jahr','monat')
 		  monaten_neu <- rbind(monaten_neu, neue_monaten_aux)
 		}
-		
+
 		cat(paste0(': ', nrow(dat), ' neue Spalten schreiben... '))
-		dbWriteTable(pgdb, name = DBI::SQL(paste0(pg_schema, ".", tabel)), 
+		dbWriteTable(pgdb, name = DBI::SQL(paste0(pg_schema, ".", tabel)),
 			value = dat, row.names = FALSE, append = TRUE)
 		cat('ok\n\n')
 		log_to_file(paste0(tabel, ': ', nrow(dat), ' neue Spalten'))
@@ -114,15 +116,15 @@ for(tabel in tabellen){
 
 # Work in progress: taeglisch Betriebsauswertung aktualisieren. Wird noch nicht ausgefuehrt.
 if(FALSE && !is.null(monaten_neu)){
-  monaten_neu <- monaten_neu |> 
+  monaten_neu <- monaten_neu |>
     filter(!is.na(jahr)) |>
     distinct() |>
     arrange(jahr, monat)
-  baus <- paste0('betriebsauswertung_', 
+  baus <- paste0('betriebsauswertung_',
                  betrieb, '.betriebsauswertung_', betrieb)
   baus_min <- querpg(
     'select jahr, monat from ', baus, ' order by jahr, monat limit 1')
-  monaten_neu <- monaten_neu |> 
+  monaten_neu <- monaten_neu |>
     filter(jahr >= baus_min$jahr && (
       jahr > baus_min$jahr || monat > baus_min$monat))
 }
@@ -131,13 +133,13 @@ if(FALSE && !is.null(monaten_neu) && nrow(monaten_neu) > 0){
   for(i in 1:nrow(monaten_neu)){
     jahr <- monaten_neu$jahr[i]
     monat <- monaten_neu$monat[i]
-    
+
     bestehend <- querpg(
-      'select count(*) from ', baus, 
+      'select count(*) from ', baus,
       ' where jahr = ', jahr,
       ' and monat = ', monat)[1,1]
     bestehend <- as.logical(bestehend)
-    
+
     if(!bestehend){
       log_to_file(str_glue('update betriebsauswertung_{betrieb}: ({jahr}, {monat})'))
       insert_betriebsauswertung(betrieb = betrieb, pg_schema = pg_schema, jahr = jahr, monat = monat, db = pgdb)
